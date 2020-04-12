c39e0788d75ba966882acc415cdef669787a848c
alek.frohlich <alek.frohlich@gmail.com>
2020-04-12 10:51:17 -0300

    wait for N players before dealing cards

--- game_client.py
+++ game_client.py
@@ -6,19 +6,22 @@ from cards import DESERIALIZED
 
 
 HOST = '127.0.0.1'
-PORT = 65431
+PORT = 65432
 
 
 class GameClient:
-    def __init__(self, server_ip):
+    def __init__(self, host):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((HOST, PORT))
 
         self.main_loop()
 
     def main_loop(self):
-        for i in range(18):
-            card = DESERIALIZED[self.socket.recv(1)]
+        while True:
+            data = self.socket.recv(1)
+            if not data:  # received all cards
+                break
+            card = DESERIALIZED[data]
             print(card)
 
 if __name__ == '__main__':
--- game_server.py
+++ game_server.py
@@ -7,39 +7,44 @@ from cards import (SUSPECTS, WEAPONS, PLACES, SERIALIZED)
 
 
 HOST = '127.0.0.1'
-PORT = 65431
+PORT = 65432
 
 
 class GameServer:
-    def __init__(self, suspects, places, weapons):
+    def __init__(self, suspects, places, weapons, num_players):
         self.suspects = sample(suspects, len(suspects))
         self.places = sample(places, len(places))
         self.weapons = sample(weapons, len(weapons))
+        self.num_players = num_players
+        self.connected = []
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((HOST, PORT))
         self.socket.listen(5)
 
-        self.chosen = self._choose()
+        self.chosen = self.choose()
         self.merged_cards = self.suspects + self.places + self.weapons
         self.merged_cards = sample(self.merged_cards, len(self.merged_cards))
 
         self.main_loop()
 
-    def _choose(self):
+    def choose(self):
         return list(map(list.pop, [self.suspects, self.places, self.weapons]))
 
-    def _push_cards(self):
-        pass
-
-    def main_loop(self):
-        # FIXME: Wait for N connections before distributting cards
-        (clientsocket, address) = self.socket.accept()
-        print(str(address) + " connected")
+    def deal_cards(self):
+        i = 0
         while len(self.merged_cards) > 0:
             card = self.merged_cards.pop()
-            clientsocket.sendall(SERIALIZED[card])
+            self.connected[i].send(SERIALIZED[card])
+            i = (i+1) % self.num_players
+
+    def main_loop(self):
+        while len(self.connected) < self.num_players:
+            (clientsocket, address) = self.socket.accept()
+            print(str(address) + " connected")
+            self.connected.append(clientsocket)
+        self.deal_cards()
 
 
 if __name__ == '__main__':
-    GameServer(SUSPECTS, PLACES, WEAPONS)
+    GameServer(SUSPECTS, PLACES, WEAPONS, 4)
